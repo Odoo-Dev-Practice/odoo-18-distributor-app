6df0d148219dcbe49faa6744f686f63c59aadd8a
Fairw <fair.gjf@hotmail.com>
2026-03-02 11:07:20 -0500

    fix(manifest): update module summary to english and register multi-company rules

--- __manifest__.py
+++ __manifest__.py
@@ -2,19 +2,20 @@
 {
     'name': "Asian Distributor",
     'summary': """
-        Módulo base para empresa importadora y distribuidora asiática.
+        Base module for Asian import and distribution companies.
     """,
     'description': """
-        Añade campos y procesos clave para importación, inventario, y ventas 
-        en un entorno de distribución mayorista.
+        Adds key fields and processes for imports, inventory, and sales 
+        tailored for wholesale distribution environments.
     """,
     'author': "Fairw",
     'category': 'Sales/Custom',
     'version': '18.0.1.0.0',
     'depends': ['base', 'stock'],
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/product_template_views.xml',
+        'security/security_groups.xml',
+        'security/ir_rule.xml',
+        'security/ir.model.access.csv',
         'views/product_template_views.xml',
     ],
     'installable': True,
